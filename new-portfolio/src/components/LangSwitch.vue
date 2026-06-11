d614ff7af7583945501406efa27eb90bc4982552
ishwar khairnar <sanketkhairnar390@gmail.com>
2026-06-11 17:00:14 +0530

    Delete new-portfolio directory

--- new-portfolio/src/components/LangSwitch.vue
+++
@@ -1,8 +0,0 @@
-<script setup lang="ts">
-import Button from "./Button.vue";
-// Lang switch disabled to keep site English-only
-</script>
-
-<template>
-  <Button variant="border" size="sm" disabled>{{ "EN" }}</Button>
-</template>
--- new-portfolio/src/content/social.ts
+++
@@ -1,6 +0,0 @@
-export const social = [
-  { url: "https://mail.google.com/mail/?view=cm&fs=1&to=sanketkhairnar390@gmail.com", name: "mail" },
-  { url: "https://github.com/ishwar1210", name: "github" },
-  { url: "https://www.linkedin.com/in/ishwar-khairnar-01aa3938b/", name: "linkedin" },
-  { url: "https://www.instagram.com/mr_sanket_khairnar_04/", name: "instagram" },
-] as const satisfies { url: string; name: "mail" | "github" | "instagram" | "linkedin" | "x" }[];
