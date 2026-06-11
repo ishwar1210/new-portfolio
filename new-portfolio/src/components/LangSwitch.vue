b5d02a4223d535ea8164df286579ccd22c931a59
Pratiksha <jmninfotech2021@gmail.com>
2026-06-11 16:32:49 +0530

    changes

---
+++ new-portfolio/src/components/LangSwitch.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+import Button from "./Button.vue";
+// Lang switch disabled to keep site English-only
+</script>
+
+<template>
+  <Button variant="border" size="sm" disabled>{{ "EN" }}</Button>
+</template>
